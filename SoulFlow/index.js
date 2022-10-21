7ac59b628b4cb797b0a8ab0dd56dd321544f24fb
DocilElm <65479309+DocilElm@users.noreply.github.com>
2022-10-20 21:48:29 -0700

    Add files via upload

--- SoulFlow/index.js
+++ SoulFlow/index.js
@@ -3,14 +3,6 @@
 const File = Java.type("java.io.File");
 import PogObject from "PogData";
 import request from '../request/index';
-const get_profile = (apikey, user_uuid) => request({url : `https://api.hypixel.net/skyblock/profiles?key=${apikey}&uuid=${user_uuid}`,headers: { 'User-Agent': ' Mozilla/5.0' }}).then(response => JSON.parse(response)).catch(error =>{ print(error);});
-const get_uuid = (username) => request({url : `https://playerdb.co/api/player/minecraft/${username}`,headers: { 'User-Agent': ' Mozilla/5.0' }}).then(response => JSON.parse(response)).catch(error =>{ print(error);});
-const get_profile_id = (user_uuid, profiles=null, apikey=null) => {
-    const getRecent = (profiles) => !profiles.profiles || !profiles.profiles.length ? null : profiles.profiles.find(a => a.selected) ?? profiles[0]
-    if (profiles) return getRecent(profiles)
-    return get_profile(apikey, user_uuid).then(profiles => getRecent(profiles)).catch(e => print(`${e}`))
-};
-const hypixel_api = (apikey, user_uuid) => request({url : `https://api.hypixel.net/skyblock/profile?key=${apikey}&profile=${user_uuid}`,headers: { 'User-Agent': ' Mozilla/5.0' }}).then(response => JSON.parse(response)).catch(error =>{ print(error);});
 const PREFIX = "&5[SoulFlow] ";
 const colors = {
     1: '§4', // Dark Red
@@ -31,20 +23,23 @@ const colors = {
     16: '§f' // White
 }
 let data = new PogObject("SoulFlow", {
-    api_key: null
+    "api_key": null,
+    "x": 0,
+    "y": 0,
+    "soulflow": 0,
+    "first_time": true
 }, ".sf_data.json");
-function fileExists(location) {
-    var file = new File(location);
-    return file.exists();
-};
-if (!fileExists("./config/ChatTriggers/modules/SoulFlow/.sf_data.json")) {  
-    data.save();
-    ChatLib.chat("")
-    new TextComponent(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Please Set Your Api Key By Doing /api new`)).chat();
-    new TextComponent(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Or By Doing /skey <key>`)).chat();
-    new TextComponent(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Join Our Discord!  &b&nDiscord&r ${colors[15]}(Click)`)).setClickAction("open_url").setClickValue("https://discord.gg/SK9UDzquEN").chat();
-    ChatLib.chat("")
-};
+register("step", () => {
+    if (data.first_time) { 
+        data.first_time = false; 
+        data.save();
+        ChatLib.chat("")
+        new TextComponent(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Please Set Your Api Key By Doing /api new`)).chat();
+        new TextComponent(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Or By Doing /skey <key>`)).chat();
+        new TextComponent(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Join Our Discord!  &b&nDiscord&r ${colors[15]}(Click)`)).setClickAction("open_url").setClickValue("https://discord.gg/SK9UDzquEN").chat();
+        ChatLib.chat("")
+    };
+}).setFps(1);
 register("chat", (key) => {
     data.api_key = key;
     data.save();
@@ -56,47 +51,35 @@ register("command", (key) => {
     ChatLib.chat(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Api Key Successfully Set!`))
     ChatLib.chat(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Please Do /ct reload!`))
 }).setName("skey");
+register("worldLoad", () => {
+    if (!data.apiKey) return
+    let uuid = Player.getUUID().replace(/-/g, "")
+    request(`https://api.hypixel.net/skyblock/profiles?key=${data.apiKey}&uuid=${uuid}`).then(sbData => {
+        sbData = JSON.parse(sbData)
+        let profile = sbData.profiles.find(a => !!a.selected)
+        if (!profile) return ChatLib.chat(ChatLib.getCenteredText(`${PREFIX}${colors[1]}Error Getting Player's Profile ID`));
+        data.soulflow = profile.members[uuid].soulflow
+        data.save()
+    }).catch(e => null)
+});
 let abc = new Gui()
-const get_display_data = () => {
-    let key = data.api_key;
-    if(key === "null" || key === null) return;
-    let username = Player.getName();
-    let soulflow
-    get_uuid(username).then(user_result => {
-        let uuid = user_result.data.player.raw_id;
-        get_profile_id(uuid, null, key).then(profile_result => {
-            let cute_name = profile_result.profile_id;
-            hypixel_api(key, cute_name).then(hypixel_result => {
-                soulflow = hypixel_result.profile.members[uuid].soulflow;
-                soulflow = soulflow || 0;
-                soulflow > 1000 ?soulflow = `${PREFIX}&2${soulflow.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`: soulflow = `${PREFIX}&4${soulflow.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
-                register("renderOverlay", () => {
-                    x = Math.round(parseInt(String(FileLib.read("./config/ChatTriggers/modules/SoulFlow/" + "x.txt")))) || 0;
-                    y = Math.round(parseInt(String(FileLib.read("./config/ChatTriggers/modules/SoulFlow/" + "y.txt")))) || 0;
-                    Renderer.drawString(`${soulflow}`, x, y, true)
-                });
-            }).catch(function(error) {
-                print(error)
-                return ChatLib.chat(ChatLib.getCenteredText(`${PREFIX}${colors[1]}Error Getting Player's Data`));
-             });
-        }).catch(function(error) {
-            print(error)
-            return ChatLib.chat(ChatLib.getCenteredText(`${PREFIX}${colors[1]}Error Getting Player's Profile ID`));
-         });
-    }).catch(function(error) {
-        print(error)
-        return ChatLib.chat(ChatLib.getCenteredText(`${PREFIX}${colors[1]}Error Getting Player's UUID`));
-     });
-}
-get_display_data();
 register("command", () => {
     abc.open();
-    ChatLib.chat(ChatLib.getCenteredText(`${PREFIX}${colors[6]}Click Anywhere In Screen To Move The Display`))
 }).setName("soulflow").setAliases(["sf"]);
 register("dragged", (dx, dy, x, y) => {
-    if (abc.isOpen()) {
-        FileLib.write("./config/ChatTriggers/modules/SoulFlow/" + "x.txt", Math.round(x));
-        FileLib.write("./config/ChatTriggers/modules/SoulFlow/" + "y.txt", Math.round(y));
-    }
+    if (!abc.isOpen()) return
+    data.x = x
+    data.y = y
+    data.save()
 });
-register("worldLoad", () => get_display_data());
+register("renderOverlay", () => {
+    soulflow = data.soulflow || 0;
+    soulflow > 1000 ?soulflow = `${PREFIX}&2${soulflow.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`: soulflow = `${PREFIX}&4${soulflow.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+    if (abc.isOpen()) {
+        const txt = "Click anywhere to move!"
+        Renderer.drawStringWithShadow(txt, Renderer.screen.getWidth()/2 - Renderer.getStringWidth(txt)/2, Renderer.screen.getHeight()/2)
+        Renderer.drawStringWithShadow(`${soulflow}`, data.x, data.y)
+    }
+    if (!data.soulflow && abc.isOpen()) return
+    Renderer.drawStringWithShadow(`${soulflow}`, data.x, data.y)
+});
